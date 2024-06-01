1a5b67aa46e7645dcdaf1425175a2af2041ac7d7
Alexandra376 <suvorovaalexandra344@gmail.com>
2024-06-02 00:37:40 +0300

    feat: add registration tests

--- API_RA/src/test/java/com/second_life/testsRA/RegistrationTest.java
+++ API_RA/src/test/java/com/second_life/testsRA/RegistrationTest.java
@@ -1,4 +1,66 @@
 package com.second_life.testsRA;
 
-public class RegistrationTest extends BaseTest{
+import com.second_life.ErrorDto;
+import com.second_life.RegistrationRequestDto;
+import com.second_life.RegistrationResponseDto;
+import io.restassured.http.ContentType;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.StringContains.containsString;
+
+public class RegistrationTest extends BaseTest {
+    RegistrationRequestDto registration = RegistrationRequestDto.builder()
+        .email("example@gmail.com")
+        .password("p@ssw0rd123")
+        .firstName("Alex")
+        .lastName("Laurentius")
+        .build();
+
+    @Test
+    public void registerSuccessTest() {
+        RegistrationResponseDto dto = given()
+                .contentType(ContentType.JSON)
+                .body(registration)
+                .when()
+                .post("/users/register")
+                .then()
+                .assertThat().statusCode(201)
+                .extract().response().as(RegistrationResponseDto.class);
+        System.out.println(dto);
+    }
+
+    @Test
+    public void registerUserWithInvalidEmailFormatTest() {
+        ErrorDto errorDto = given()
+                .contentType(ContentType.JSON)
+                .body(RegistrationRequestDto.builder()
+                        .firstName("Luna")
+                        .lastName("Klein")
+                        .email("noextrasanytime.com")
+                        .password("secure#passw0rd").build())
+                .post("/users/register")
+                .then()
+                .assertThat().statusCode(400)
+                .body(containsString("Email is not valid"))
+                .extract().response().as(ErrorDto.class);
+        System.out.println(errorDto.getMessage());
+    }
+
+    @Test
+    public void registerUserWithExistEmailTest() {
+        ErrorDto errorDto = given()
+                .contentType(ContentType.JSON)
+                .body(RegistrationRequestDto.builder()
+                .firstName("Bella")
+                .lastName("Leo")
+                .email("example@gmail.com")
+                .password("secure#passw0rd").build())
+            .post("/users/register")
+            .then()
+            .assertThat().statusCode(409)
+            .body(containsString("Email example@gmail.com already exists"))
+            .extract().response().as(ErrorDto.class);
+        System.out.println(errorDto.getMessage());
+    }
 }
